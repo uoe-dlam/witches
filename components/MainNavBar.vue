a40f86bfc4636e2f7affaa8a3815bae521606560
Richard Lawson <richard.lawson@ed.ac.uk>
2019-09-02 13:55:30 +0100

    make sure menu doesn't wrap

--- components/MainNavBar.vue
+++ components/MainNavBar.vue
@@ -1,24 +1,24 @@
 <template>
     <div id="main-nav">
         <nav class="flex items-center justify-between flex-wrap bg-white p-5 w-full">
-          <div class="flex items-center mr-6">
-              <h1 class="text-2xl sm:text-4xl mr-4">Witches</h1><img src="/images/witches-cluster-composite.png" alt="logo" width="100" >
+          <div class="flex items-center mr-3">
+              <h1 class="text-xl sm:text-4xl mr-4">Witches</h1><img src="/images/witches-cluster-composite.png" alt="logo" width="100" >
               <img class="block lg:hidden" src="~/assets/main-logo-badge.png" alt="logo" width="75">
           </div>
-          <div class="block lg:hidden">
+          <div class="block xl:hidden">
             <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey hover:text-grey hover:border-grey-dark">
               <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
           </div>
-          <div :class="open ? 'block': 'hidden'" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div :class="open ? 'block': 'hidden'" class="w-full block flex-grow xl:flex xl:items-center xl:w-auto">
             <div class="text-md lg:flex-grow">
                 <main-nav-bar-item v-for="link in links" :url="link.url" :key="link.url" :external="link.external" :subpages="link.subpages">
                     {{link.name}}
                 </main-nav-bar-item>
             </div>
           </div>
-          <div class="flex items-center text-white mr-6 hidden lg:block">
-            <img src="~/assets/main-logo.png" alt="logo" width="250">
+          <div class="flex items-center text-white mr-6 hidden xl:block">
+            <img src="~/assets/main-logo.png" alt="logo" width="225">
           </div>
         </nav>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+      screens: {
+          'sm': '640px',
+          // => @media (min-width: 640px) { ... }
+
+          'md': '768px',
+          // => @media (min-width: 768px) { ... }
+
+          'lg': '1024px',
+          // => @media (min-width: 1024px) { ... }
+
+          'xl': '1350px',
+          // => @media (min-width: 1280px) { ... }
+      }
+  },
   variants: {},
   plugins: []
 }
+
