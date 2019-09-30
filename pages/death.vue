162d83fbe8797e5751b7d82713125587e22223c5
Richard Lawson <richard.lawson@ed.ac.uk>
2019-09-30 14:57:52 +0100

    add attribution update favicon

--- pages/death.vue
+++ pages/death.vue
@@ -131,7 +131,7 @@ export default {
         filters: false,
         sparqlUrl: 'https://query.wikidata.org/sparql',
         url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        attribution: 'Historical Maps Layer, 1919-1947 from the <a href="http://maps.nls.uk/projects/api/">NLS Maps API</a>',
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>. Historical Maps Layer, 1919-1947 from the <a href="http://maps.nls.uk/projects/api/">NLS Maps API</a>',
         zoom: 7,
         center: [55.95, -3.198888888],
         wikiPages: [],
