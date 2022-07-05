89da178dc0bdcc20258eb05879babad2f74ad227
Josep Reyero <josepreyero@gmail.com>
2022-07-05 09:45:17 +0100

    Moved filter data to store.

--- store/filters.js
+++ store/filters.js
@@ -37,6 +37,216 @@ export const state = () => ({
     noWiki: {
       label: "No wiki", active: true,
       iconUrl: '/images/witch-single-orange.png'
+    },
+    "middling": {
+      "label": "middling",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "pauper": {
+      "label": "pauper",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
+    },
+    "unknown": {
+      "label": "unknown",
+      "active": true,
+      "iconUrl": "/images/witch-single-pink.png"
+    },
+    "working poor": {
+      "label": "working poor",
+      "active": true,
+      "iconUrl": "/images/witch-single-red.png"
+    },
+    "vagrant": {
+      "label": "vagrant",
+      "active": true,
+      "iconUrl": "/images/witch-single-brown.png"
+    },
+    "nobility": {
+      "label": "nobility",
+      "active": true,
+      "iconUrl": "/images/witch-single-green.png"
+    },
+    "upper class": {
+      "label": "upper class",
+      "active": true,
+      "iconUrl": "/images/witch-single-pale-blue.png"
+    },
+    "Laird": {
+      "label": "Laird",
+      "active": true,
+      "iconUrl": "/images/witch-single-yellow.png"
+    },
+    "unknown": {
+      "label": "unknown",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "vagrant": {
+      "label": "vagrant",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
+    },
+    "domestic worker": {
+      "label": "domestic worker",
+      "active": true,
+      "iconUrl": "/images/witch-single-pink.png"
+    },
+    "midwife": {
+      "label": "midwife",
+      "active": true,
+      "iconUrl": "/images/witch-single-red.png"
+    },
+    "Christian minister": {
+      "label": "Christian minister",
+      "active": true,
+      "iconUrl": "/images/witch-single-brown.png"
+    },
+    "courier": {
+      "label": "courier",
+      "active": true,
+      "iconUrl": "/images/witch-single-green.png"
+    },
+    "weaver": {
+      "label": "weaver",
+      "active": true,
+      "iconUrl": "/images/witch-single-pale-blue.png"
+    },
+    "cunning folk": {
+      "label": "cunning folk",
+      "active": true,
+      "iconUrl": "/images/witch-single-yellow.png"
+    },
+    "shopkeeper": {
+      "label": "shopkeeper",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "miller": {
+      "label": "miller",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
+    },
+    "laborer": {
+      "label": "laborer",
+      "active": true,
+      "iconUrl": "/images/witch-single-pink.png"
+    },
+    "metalsmith": {
+      "label": "metalsmith",
+      "active": true,
+      "iconUrl": "/images/witch-single-red.png"
+    },
+    "healer": {
+      "label": "healer",
+      "active": true,
+      "iconUrl": "/images/witch-single-brown.png"
+    },
+    "loadman": {
+      "label": "loadman",
+      "active": true,
+      "iconUrl": "/images/witch-single-green.png"
+    },
+    "maltman": {
+      "label": "maltman",
+      "active": true,
+      "iconUrl": "/images/witch-single-pale-blue.png"
+    },
+    "blacksmith": {
+      "label": "blacksmith",
+      "active": true,
+      "iconUrl": "/images/witch-single-yellow.png"
+    },
+    "merchant": {
+      "label": "merchant",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "occultist": {
+      "label": "occultist",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
+    },
+    "mealmaker": {
+      "label": "mealmaker",
+      "active": true,
+      "iconUrl": "/images/witch-single-pink.png"
+    },
+    "teacher": {
+      "label": "teacher",
+      "active": true,
+      "iconUrl": "/images/witch-single-red.png"
+    },
+    "farmer": {
+      "label": "farmer",
+      "active": true,
+      "iconUrl": "/images/witch-single-brown.png"
+    },
+    "tailor": {
+      "label": "tailor",
+      "active": true,
+      "iconUrl": "/images/witch-single-green.png"
+    },
+    "stabler": {
+      "label": "stabler",
+      "active": true,
+      "iconUrl": "/images/witch-single-pale-blue.png"
+    },
+    "fisher": {
+      "label": "fisher",
+      "active": true,
+      "iconUrl": "/images/witch-single-yellow.png"
+    },
+    "piece work": {
+      "label": "piece work",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "brewster": {
+      "label": "brewster",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
+    },
+    "coal miner": {
+      "label": "coal miner",
+      "active": true,
+      "iconUrl": "/images/witch-single-pink.png"
+    },
+    "slater": {
+      "label": "slater",
+      "active": true,
+      "iconUrl": "/images/witch-single-red.png"
+    },
+    "nurse": {
+      "label": "nurse",
+      "active": true,
+      "iconUrl": "/images/witch-single-brown.png"
+    },
+    "School Master": {
+      "label": "School Master",
+      "active": true,
+      "iconUrl": "/images/witch-single-green.png"
+    },
+    "henwife": {
+      "label": "henwife",
+      "active": true,
+      "iconUrl": "/images/witch-single-pale-blue.png"
+    },
+    "cook": {
+      "label": "cook",
+      "active": true,
+      "iconUrl": "/images/witch-single-yellow.png"
+    },
+    "sailor": {
+      "label": "sailor",
+      "active": true,
+      "iconUrl": "/images/witch-single-blue.png"
+    },
+    "creelman": {
+      "label": "creelman",
+      "active": true,
+      "iconUrl": "/images/witch-single-orange.png"
     }
   }
 });
