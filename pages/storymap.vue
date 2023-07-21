9c62c424dcee49acee2a616228525ede2611c530
Ruby Imrie <s2056921@ed.ac.uk>
2023-07-21 14:09:56 +0100

    embedding storymap instead of external link

--- pages/storymap.vue
+++ pages/storymap.vue
@@ -1,10 +1,6 @@
 <template>
-  <div id="main-content" class="text-wrapper">
-    <h1 class="text-sm md:text-xl lg:text-2xl mb-3">The Story of Isobel Young</h1>
-    <p class="mb-3">There are many, many stories contained within the Survey of Scottish Witchcraft database.</p>
-    <p class="mb-3">This is the story of Isobel Young (c.1565) who was the wife of a tenant farmer residing in the village of East Barns in the parish of Dunbar, Lothian, Scotland. She was tried (c. February 1629) for practicing witchcraft.</p>
-    <p class="mb-3">Follow the link below to view the leaflet storymap about Isobel Young's witchcraft investigation</p>
-    <p class="mb-3"><b><a href="https://emcandre.github.io/leaflet-storymaps-with-google-sheets/" target="_blank">View the leaflet storymap</a></b></p>
+  <div style="height: 100%;">
+    <iframe src="https://emcandre.github.io/leaflet-storymaps-with-google-sheets/" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
   </div>
 </template>
 
