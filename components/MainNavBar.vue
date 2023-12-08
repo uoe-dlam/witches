35f7913f0c2a48334aecb197f1a7265876f1c871
Ruby Imrie <s2056921@ed.ac.uk>
2023-12-08 13:54:57 +0000

    adding the books

--- components/MainNavBar.vue
+++ components/MainNavBar.vue
@@ -80,6 +80,7 @@
            { name : 'References', url : '/reference', external: false },
            { name : 'Resources', url : '/resources', external: false },
            { name : 'Contact', url : '/contact', external: false },
+           { name : 'Further Reading', url : '/furtherreading', external: false }
          ] },
          { name: 'Blog', url: 'https://blogs.ed.ac.uk/witchcraft_visualisation/', external: true, subpages : [] }
        ]
---
+++ pages/furtherreading.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="main-content" class="text-wrapper">
+    <h1 class="text-center text-2xl md:text-3xl lg:text-4xl mb-6">Further Reading</h1>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Satan and the Scots: The Devil in Post-Reformation, c1560-1700</p>
+    <p class="mb-3">Michelle D.Brock (Farnham: Ashgate, 2016)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">The Scottish Witch-Hunt in Context</p>
+    <p class="mb-3">Julian Goodare (Manchester: Manchester University Press,2002)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Scottish Witches and Witch-Hunters</p>
+    <p class="mb-3">Julian Goodare (Basingloke: Palgrave Macmillan,2013)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Witchcraft and Belief in Early Modern Scotland</p>
+    <p class="mb-3">Julian Goodare, Lauren Martin and Joyce Miller eds. (Basingloke: Palgrave Macmillan,2008)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Witchcraft and Folk Belief in Early Modern Scotland</p>
+    <p class="mb-3">Lizanne Henderson (Basingloke: Palgrave Macmillan,2016)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Witchcraft and Folk Belief in the Age of Enlightment: Scotland, 1670-1740</p>
+    <p class="mb-3">Lizanne Henderson (Basingloke: Palgrave Macmillan,2016)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Enemies of God: The Witch-Hunt in Scotland</p>
+    <p class="mb-3">Christina Larner (London: Chatto & Windus, 1981)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Witch-Hunting in Scotland: Law, Politics and Religion</p>
+    <p class="mb-3">Brian P.Levack (London: Routledge, 2008)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">The Witches of Fife: Witch-Hunting in a Scottish Shire</p>
+    <p class="mb-3">Stuart Macdonald (East Linton: Tuckwell,2001)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">The Visons of Isobel Gowdie: Magic, Witchcraft and Dark Shamanism in Seventeenth-Century Scotland</p>
+    <p class="mb-3">Emma Wilby (Brighton: Sussex Academic Press, 2010)</p>
+    <p class="mt-6 mb-3 text-lg md:text-xl lg:text-xl font-medium">Witches of the North: Scotland and Finmark</p>
+    <p class="mb-3">Liv Helene Willumson (Leidon: Brill, 2013)</p>
+  </div>
+</template>
+
+<script>
+
+
+ export default {
+   data: () => ({
+
+   }),
+   methods: {
+
+   },
+   computed : {
+
+   },
+   mounted: function() {
+   },
+   layout: 'basic'
+ };
+</script>
+
+<style scoped>
+.custom-margin {
+  margin-left: 8%;
+  margin-right: 8%;
+}
+
+
+</style>
