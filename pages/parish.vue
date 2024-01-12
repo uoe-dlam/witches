d595c1172a429a84e28f757f9666816a69be153a
Ruby Imrie <s2056921@ed.ac.uk>
2024-01-12 12:27:48 +0000

    making parish map take up full screen on mobile

--- pages/parish.vue
+++ pages/parish.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="height: 100%;">
     <div id="main-content" class="pl-5 pr-5 pt-3 pb-3">
 
       <div class="flex content-start items-center">
@@ -13,8 +13,34 @@
       </div>
 
     </div>
-    <div>
-      <style>.embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Parish Boundary Residence Location of Accused Witches" src="//edinuniv.maps.arcgis.com/apps/Embed/index.html?webmap=379dbfae89704e31ba67e1811bc33605&extent=-7.4748,55.2089,0.6386,57.8114&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=false&theme=dark"></iframe></div>
+    <div style="height: 90%;">
+      <style>
+        .embed-container {
+          position: relative;
+          padding-bottom: 100%; /* Set padding-bottom to 100% to maintain aspect ratio */
+          height: 0;
+          max-width: 100%;
+        }
+
+        .embed-container iframe,
+        .embed-container object,
+        .embed-container iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+
+        small {
+          position: absolute;
+          z-index: 40;
+          bottom: 0;
+          margin-bottom: -15px;
+        }
+      </style>
+      <div class="embed-container" style="height:100%;">
+        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Parish Boundary Residence Location of Accused Witches" src="//edinuniv.maps.arcgis.com/apps/Embed/index.html?webmap=379dbfae89704e31ba67e1811bc33605&extent=-7.4748,55.2089,0.6386,57.8114&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=false&theme=dark"></iframe></div>
     </div>
   </div>
 </template>
