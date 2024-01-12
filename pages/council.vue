71813054ad693b1279cf9e721a80f51e6726b3c0
Ruby Imrie <s2056921@ed.ac.uk>
2024-01-12 12:34:54 +0000

    making council map take up full screen on mobile

--- pages/council.vue
+++ pages/council.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="height: 100%;">
     <div id="main-content" class="pl-5 pr-5 pt-3 pb-3">
       <div class="flex content-start items-center">
         <h1 class="text-sm md:text-xl lg:text-2xl">Number of Trials Recorded in Each Modern Council Authority</h1>
@@ -12,8 +12,35 @@
       </div>
 
     </div>
-    <div>
-      <style>.embed-container {position: relative; padding-bottom: 71%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><iframe width="900" height="640" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Choropleth Map of Trial Location" src="//edinuniv.maps.arcgis.com/apps/Embed/index.html?webmap=18afc056bff34ae3a7d76fade8cd195d&extent=-5.6973,55.3553,-1.6406,56.674&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=false&theme=dark"></iframe></div>
+    <div style="height: 90%;">
+      <style>
+        .embed-container {
+          position: relative;
+          padding-bottom: 100%; /* Set padding-bottom to 100% to maintain aspect ratio */
+          height: 0;
+          max-width: 100%;
+        }
+
+        .embed-container iframe,
+        .embed-container object,
+        .embed-container iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+
+        small {
+          position: absolute;
+          z-index: 40;
+          bottom: 0;
+          margin-bottom: -15px;
+        }
+      </style>
+      <div class="embed-container" style="height:100%;">
+        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Choropleth Map of Trial Location" src="//edinuniv.maps.arcgis.com/apps/Embed/index.html?webmap=18afc056bff34ae3a7d76fade8cd195d&extent=-5.6973,55.3553,-1.6406,56.674&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=false&theme=dark"></iframe>
+      </div>
     </div>
   </div>
 </template>
