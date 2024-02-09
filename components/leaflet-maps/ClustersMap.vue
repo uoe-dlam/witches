48dc8339a12bb0539d3bdc93fead032240ddb34b
Ruby Imrie <s2056921@ed.ac.uk>
2024-02-09 13:53:26 +0000

    converting to julian when full precision, shortening when low presicion

--- components/leaflet-maps/ClustersMap.vue
+++ components/leaflet-maps/ClustersMap.vue
@@ -21,8 +21,7 @@
 
               <div class="font-semibold text-base">{{ witch.name }}</div><br>
               <div>
-                <b>Investigation Date:</b> {{ getJulianDate(witch.investigationDates[1],witch.precision) }}<br>
-                <b>Precision:</b> {{ witch.precision }}<br>
+                <b>Investigation Date:</b> {{ getFormatDate(witch.investigationDates[1],witch.precision) }}<br>
               </div>
 
               <div v-for="standardAttribute in getStandardAttributesWithValue(witch)">
@@ -197,24 +196,37 @@
 
        return optionalsWithValue
      },
-     getJulianDate: function (date,precision){
-      // these witches shouldn't be converted because there full date isn't known, they will appear as "01/01/1632" which just means the year 1632, so therefore if converted would give the wrong date
-      //const noconversion = ["Thom Cockburn","Jonat Drummond","Archie Farquhars","Margaret Jo","Jonet Fairlie","Ane Nairn","Marion Ranking","Katherine Muirhead","Andrew Abel","Helen Widdrow","Husband of Elizabeth Rennie","Elizabeth Lawsone","Issobell Ritchardsone","Agnes Murray","Jeane Deanes","Begis Bathcat","Euphame Haliburton","Marion Hutson","Issobell Hutson","George Hutson","Isobel Wilson","Elspeth Baillie","Helene Deanes","Issobell Cairnes","Helen Sharpe","Adam Harlaw","Jonet Wast","Margaret Keltie","Marioun Tailyeor","Adam Robertson","Margaret Kinard","Margaret Fyfe","Elizabeth Dempster","Brand","Agnes Drysdale","Margaret Beveridge","Agnes Beveridge","Christian Young","Margaret McNish","Margaret Lauder","Margaret Young","Christian Crieff","Isabel Condie","Janet Hird","Agnes Sharp","Gilleis Hutton","Elspet Alexander","Agnes Allene","Helen Alexander","Mary Rynd","Jonet Mathie","Mary Mitchell","John Gray","Janet McNair","Thomas Mitchell","Marion Phin","McQuicken","McRorie","Donald Moir","John Lindsay","Agnes Chisolm","Agnes Urich","Janet Walker","Issobel Bairdie","Agnes Gourlay","Janet Castell","Robert Bruce","Janet Galloway","Thomas Humbell","Issobel Fordell","Agnes Gaston","Jonet Birnie","Janet Anderson","Maggy Osborne","Marion Mackbeath","Margaret Cant","Elspeth Craiche","Effie Rosie","John Wauch","Jonet Anderson","John Tailzour","Janet Huit","Margaret Duff","Jonat nein giblie Gow","Margaret Tamsone","Janet Finnie","James McPhee","Janet Conochie","Meggie Cowie","Goodaile","Elspet Smith","Agnes Mor Nin Vick Eanglaish","Isabel Davidson","Margaret Kirkwood","Ann Nin William","Elspeth Kirkland","Marion Purdie","Helen Eliot","William Easone","William Simpson","Marion Dickson","Isobel Dickson","Marion Herbertson","Margaret Laird","Marion Lillie","Margaret Polwart","Alison Nisbet","Jean Hart","Jonnet Buttar","Ellen King","Barbara Tulloch","Agnes Fergusson","Neville","Helen Andrew","Jannet Pyper","Margaret Oisone","Margaret Nin Gilbert","Margaret Drummond","Janet Horne","William Stewart","Patrick Macgueire","Katherine Fergus","Jonat Davidsone","Issobell Oige","Helene Makkie","John Wilson","Janet Smyth","Margaret Atkin","Janet Allane","William Murray","Nicole","Jon Bayne","Elspet Cant","Gelis Gray","Malcome Toir","Kathrene Smyth","George Semple","Issobell Johnestowne","Agnes Anstruther","Catherine McNiven","Geordie Archobald","Helen Wallas","Bessie Finlaysoune","Marion Marnow","Agnes Meldrum","Bessie Archer","Janet Weill","Issobell Monro","Susanna Chancelar","Robert Simpson","James Hall","Thomas Murray","Issobell Traye","Agnes Nemo","Tibbie Smart","Graham","Katherine Kerse","Malie Macwatt","Isabel Cuthbertson","Gilbert Robisone","Lillias Bertram","Agnes Muircone","Katherine Mitchell","Marion Fisher","Agnes Robert","Margaret Balfour","Bessie Carnochan","Kathren Wallace","Janet Anderson","Grissel Morris","Margaret Brand","Katherine Elder","Agnes Kirk","Margaret Donaldson","Isobel Miller","Agnes Wallace","Jonet Tulloch","Agnes Muresone","Christian Roch","Janet Bruce","Margaret Ramsay","Bessie Mason","Marion Ramsay","Rosina McCoskrie","(wife of John) McNaught","Margaret Sinclair","Margaret Clerk","Janet McGoane","Janet Mowbray","Margaret Brown","Catherin Thompson","Marion Dauline","Marion Stein","Seweis","Marg Donald","Parker","William Chrictoun","Isabell Peacock","Isobel Gairdner","Janet Thomson","Janet Brown","James Henrison","Marion Henrison","Isobel Thompson","Jonet Bennet","Marione McNab","Maly Purdie","Murray","Marg Robisone","Jean Campbell","Barbara Milne","Margaret Carvie","Barbara Horniman","John Rind","Ketheren Portour","Agnes Sparke","Isobell Shyrie","Bessie Croket","George Patersone","Meg Lawson","Janet McRobert","Elizabeth Dick","Molly Redmond","Tonderghee","Grissel Anderson","Robert Bainzie","Euphan Stirt","Janet McMurray","Anna Wood","Elspeth Williamson","Lillias Adie","White","Mary Wilson","Janet Whyte","Mary McNarin","Mary Stewart","John McNairn","Alexander Deuart","Kathrine Taylor","Isobel Anderson","Margaret Ogilvy","Sarah Johnson","M'huistan","Margaret Callum","Isbell Dairsie","Christine Dote","Margaret Myrton","Agnes Anstruther","Marioun Tailzeour","Geillis Gray","Maggie","wife of William Barton","Margaret Patoune","Margaret Laurymer","Janet Tucidie","Janet Allane","Janet Bowis","Effie Pothif","wife of Robert Wir","Janet Coldane","Janet Robison","Elspeth Chib","Bessie Nicoson","Margaret Waldon","Marin Coran","Agnes Williamsone","Helen Reid","Patrick Meikle","Margaret Blak","John Homme","Janet Pook","Issobell Cathie","Susanna Bannytyne","Christian Blaik","Jonnet Kempe","Anna Pilmure","Marion Blaik","Jeane Martin","Jean Kerse","Janet Symson","Bessie Lamb","Jon Russell","Jon Lyll","wife of Thomas Hay","Marion Cumming","George Binnie","George Lacost","Helen Nicolson","David Schankis","Neving","Thomas Weir","Cristian Brake","James Anderson","Janet Anderson","Jeane Laying","James Beverley","Grissel McIlney","Bessie Paine","Margaret McGuffock","Grissell Rae","Jonet Howat","Janet Robertson","Jonet Losk","Hopkin","Janet Fisher","Helen Wilson","Joan Graham","Marion McRae","Issabell Reid","Catharin Lorimer","Jean Gray","Jennet Broun","John Black","Margaret Wilkin","John Glass","John Paterson","Jean Woodrow","Elspeth Rule","Christian Bonn","Geddoch McAlester Boyne","Bornlie","Gilbert Mowat","Patrick Barnett","Bessie Andro","Agnes Gune","Jonet Dow","Marshall Sutherland","Adam Barnie","Marione Kenock","John Harper","William McAngus","Thomas Mrgillie Lukell","Margaret Nakoch","Gretchach Unchach","Henrie Lyell","Donald McChereich","Jean Rutherd","Elspeth Chousley","Christian Wilkieson","Katie","Margaret Hay","Isobel Insh","Johne Young","Charles Wat","Janet Campbell","Margaret Nidrie","Margaret Dalgleish","Margaret Garvie","Barbara Honeyman","Jonet Hood","James Og","Elspet Strachund","Christiane Arcly","George Milton","Agnes Nevie","Margrat NcWilliam","wife of Wilsone","Katherine Gilmore","Issobell Pain","Marion Wood","James Jervie","Jonet Mackley","Agnes Baxter","wife of Alexander Aitkin","Mathew Connell","Mareoun","Scota Bess","Margaret Wick","Eupham Wright","Adam Donaldson","Jonet Durie","Elspeth Durie","Agnes Anderson","Issobell Davidson","Bessie Dock","Euphame Henderson","Agnes Mertene","Bessie Smith","Marie Dick","Margaret Gray","Margaret Lummisdane","Issobell Chrystie","Marie Broun","Helen Wood","Jonet Inglyses","Jonet Lummisdane","Issobell Oster","Beatrix Dolt","Bessie Boyd","Jonet Kennedy","wife of George Mot","Jonet Young","Bessie Bell","Janet M'Allexander","Effie McBrwme","Helen Pennant","Agnis Quhytelaw","Elizabeth Hunter","Agnes Peramorris","Geilis Feirour","Jonet Barroman","Agnes Gordoun","Bessie Ramsay","Bessie Lamb","Gelis Durye","Bessye Brodye","Maldye Sturrok","Issobell Robertsoun","Jonet Gardyn","Dame Logye","Johnne Stevin","Henrye Stevin","wife of Johnne Durwart","Jonet Harbertson","Jonet Newton","Cristiane Twych","Betie Wobster","Jonet Duncane","Katherine Cusing","Issobell Sutye","Law","Fischearis Docter","James Ochterlonyes","Margaret Mowbray","Dowglass","Cristeane Incheot","Lyvelyke","James Kilgour","Effie Roger","Cristeane Johnnestoun","Jonet Curchan","James Chalmer","Catherine Campbell","Cristeane Jak"];
-      
-      if (precision != 'unknown'){
-        if (Number(precision)<11){
-          return date; //return same date if in noconversion
-        }  
-      }
-      
+     getFormatDate: function (date,precision){
+
       let [day, month, year] = date.split('/');
       day = Number(day);
       month = Number(month)
       year = Number(year);
       let currentDate = new Date(year, month - 1, day);
-      let earlierDate = new Date(currentDate);
-      const cutoffDate = new Date(1700,3,1)
+      
+      
+      //the precision of the dates can either be or 9 11(full day/month/year),10(month/year),9(year)
+      
+      // If we don't know the day, month, and year, then wikidata precision will be less than 11 and the dates don't need Julian conversion
+      if (precision === '10') {
+          // Then return the date with month and year only
+          let dateString = `${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/` + `${currentDate.getFullYear()}`;
+          return dateString;
+      }  
+          // If only year is known
+      if (precision === '9')  {
+          // Then return the date with year only
+          let dateString = `${currentDate.getFullYear()}`;
+          return dateString;
+      }
+        
+      
 
+      //if we have the full date (day,month,year) them the date needs to be converted into Julian
+      const cutoffDate = new Date(1700,3,1) // the date where the difference between julian and gregorian cjanges from 10 to 11
+      let earlierDate = new Date(currentDate);
+
+      // JULIAN CONVERSION - https://en.wikipedia.org/wiki/Conversion_between_Julian_and_Gregorian_calendars
       if (currentDate < cutoffDate) {
         earlierDate.setDate(currentDate.getDate()-10);
       }
@@ -222,13 +234,14 @@
         earlierDate.setDate(currentDate.getDate()-11);
       }
 
+      // covert back to string
       const earlierDateString =
         `${earlierDate.getDate().toString().padStart(2, '0')}/` +
         `${(earlierDate.getMonth() + 1).toString().padStart(2, '0')}/` +
         `${earlierDate.getFullYear()}`;
 
       return earlierDateString;
-     }
+    }
    },
    computed: {
      iconAnchor : function () {
