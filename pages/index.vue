739590046dd737e5b570ecc76566284f83b3ba9c
Ruby Imrie <s2056921@ed.ac.uk>
2024-02-16 12:30:44 +0000

    change to correct number

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@ export default {
   data: () => ({
     pageInfo: {
       title: "Witchcraft Prosecutions In Time and Place",
-      html: '<div>This map shows the geographical residence location for each accused witch in Scotland taken from the Survey of Scottish Witchcraft Database. Out of the <b class="font-bold">3212</b> accused witches whose names are known, the residence for <b class="font-bold">3142</b> witches has been located. The majority of the residences are accurately located down to the precise settlement, while others range from parish to county depending on the records surviving for each accused witch. There is a total of 821 different locations recorded in the database; all but 25 of these have been identified. The remaining unidentified place-names are currently recorded as \‘County of’\ on the map.</div>',
+      html: '<div>This map shows the geographical residence location for each accused witch in Scotland taken from the Survey of Scottish Witchcraft Database. Out of the <b class="font-bold">3212</b> accused witches whose names are known, the residence for <b class="font-bold">3145</b> witches has been located. The majority of the residences are accurately located down to the precise settlement, while others range from parish to county depending on the records surviving for each accused witch. There is a total of 821 different locations recorded in the database; all but 25 of these have been identified. The remaining unidentified place-names are currently recorded as \‘County of’\ on the map.</div>',
       footer: "witches.is.ed.ac.uk",
       confirmButtonText: "Close",
       type: "info",
