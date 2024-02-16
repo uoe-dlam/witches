a642c05b5b02367e1ab0401c9622df1bfa7cfb51
Ruby Imrie <s2056921@ed.ac.uk>
2024-02-16 11:46:47 +0000

    fixing nav bar

--- components/MainNavBarItem.vue
+++ components/MainNavBarItem.vue
@@ -3,11 +3,11 @@
     <a :href="url" class="block mt-4 xl:pt-1 xl:pb-1 xl:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" v-if="external">
       <slot></slot>
     </a>
-    <nuxt-link :to="url" class="block xl:pt-1 xl:pb-1 mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" :class="{ 'menuBarItemActive' :  isSubpageActive(subpages)}" v-else>
+    <nuxt-link :to="url" class="block xl:pt-1 xl:pb-1 mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" :class="{ 'menuBarItemActive' : ( isPageActive(url) || isSubpageActive(subpages))}" v-else>
       <slot></slot>
     </nuxt-link>
     <div v-if="subpages.length > 0" class="block dropdown-content lg:absolute lg:hidden lg:bg-gray-200 lg:shadow-xl text-gray-500">
-      <a v-for="(page, index) in subpages" :href="page.url" class="block pt-3 pb-0 pl-2 pr-2 lg:p-2 hover:text-black" :class="{ 'subMenuItemActive' : isSubActive(page.url)}">{{ page.name }}</a>
+      <a v-for="(page, index) in subpages" :href="page.url" class="block pt-3 pb-0 pl-2 pr-2 lg:p-2 hover:text-black" :class="{ 'subMenuItemActive' : isPageActive(page.url)}">{{ page.name }}</a>
     </div>
   </span>
 </template>
@@ -17,7 +17,7 @@
  export default {
    props: ['url', 'external', 'subpages'],
    methods: {
-     isSubActive : function(url){
+     isPageActive : function(url){
        return url === this.$route.path || url + "/" === this.$route.path ;
      },
      isSubpageActive: function(subpages){
