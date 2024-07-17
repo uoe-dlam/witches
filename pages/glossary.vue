4d2a4e96c401a23dab1aaee1454c39de21acce58
Ruby Imrie <s2056921@ed.ac.uk>
2024-07-17 16:28:17 +0100

    formatting

--- pages/glossary.vue
+++ pages/glossary.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-      <h1>Glossary</h1>
-      <ul>
-        <li v-for="(item, index) in sortedGlossary" :key="index">
-          <h2>{{ item.word }}</h2>
-          <p><strong>Category:</strong> {{ item.category }}</p>
-          <p><strong>Definition:</strong> {{ item.definition }}</p>
-        </li>
-      </ul>
+      <h1 class="text-2xl text-center mb-3">Glossary</h1>
+      <div class="ml-10 mr-10">
+      <template v-for="letter in sortedLetters">
+        <h2>{{ letter }}</h2>
+        <hr>
+        <ul class="mb-3">
+          <li v-for="item in sortedGlossary[letter]" :key="item.word">
+            <h3>{{ item.word }}</h3>
+            <p><strong>Category:</strong> {{ item.category }}</p>
+            <p><strong>Definition:</strong> {{ item.definition }}</p>
+          </li>
+        </ul>
+      </template>
+    </div>
     </div>
   </template>
   
@@ -22,10 +28,32 @@
     },
     computed: {
       sortedGlossary() {
-        return this.glossary.slice().sort((a, b) => {
-          return a.word.localeCompare(b.word);
+        // Create an object to store groups of glossary items by letter
+        let grouped = {};
+        
+        // Loop through glossary items and group them by the first letter of 'word'
+        this.glossary.forEach(item => {
+          let letter = item.word.charAt(0).toUpperCase(); // Get the first letter and convert to uppercase
+          if (!grouped[letter]) {
+            grouped[letter] = [];
+          }
+          grouped[letter].push(item);
         });
+        
+        // Sort groups by letter
+        const sortedGroups = {};
+        Object.keys(grouped).sort().forEach(key => {
+          sortedGroups[key] = grouped[key].sort((a, b) => {
+            return a.word.localeCompare(b.word);
+          });
+        });
+        
+        return sortedGroups;
       },
+      sortedLetters() {
+        // Return an array of sorted letters from sortedGlossary object keys
+        return Object.keys(this.sortedGlossary).sort();
+      }
     },
   };
   </script>
