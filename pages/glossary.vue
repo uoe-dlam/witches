09bca9402290787422d090b54f4ceac755312901
Ruby Imrie <s2056921@ed.ac.uk>
2024-07-17 16:10:15 +0100

    sorting alphabetically

--- pages/glossary.vue
+++ pages/glossary.vue
@@ -2,7 +2,7 @@
     <div>
       <h1>Glossary</h1>
       <ul>
-        <li v-for="(item, index) in glossary" :key="index">
+        <li v-for="(item, index) in sortedGlossary" :key="index">
           <h2>{{ item.word }}</h2>
           <p><strong>Category:</strong> {{ item.category }}</p>
           <p><strong>Definition:</strong> {{ item.definition }}</p>
@@ -13,13 +13,20 @@
   
   <script>
   import glossaryJSON from "../static/glossary.json";
+  
   export default {
     data() {
       return {
         glossary: glossaryJSON,
       };
     },
-
+    computed: {
+      sortedGlossary() {
+        return this.glossary.slice().sort((a, b) => {
+          return a.word.localeCompare(b.word);
+        });
+      },
+    },
   };
   </script>
   
