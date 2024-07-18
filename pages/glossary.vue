7287172ec3aff918ee86c73d8dfe981f72601131
Ruby Imrie <s2056921@ed.ac.uk>
2024-07-18 09:12:16 +0100

    making letters expandable

--- pages/glossary.vue
+++ pages/glossary.vue
@@ -1,60 +1,79 @@
 <template>
-    <div>
-      <h1 class="text-2xl text-center mb-3">Glossary</h1>
-      <div class="ml-10 mr-10">
-      <template v-for="letter in sortedLetters">
-        <h2>{{ letter }}</h2>
+  <div>
+    <h1 class="text-2xl text-center mb-3">Glossary</h1>
+    <div class="ml-10 mr-10">
+      <div v-for="letterObj in letters" :key="letterObj.letter">
+        <h2 @click="toggleLetter(letterObj.letter)" class="cursor-pointer">
+          {{ letterObj.letter }} <span v-if="letterObj.expanded">-</span><span v-else>+</span>
+        </h2>
         <hr>
-        <ul class="mb-3">
-          <li v-for="item in sortedGlossary[letter]" :key="item.word">
+        <ul v-show="letterObj.expanded" class="mb-3">
+          <li v-for="item in sortedGlossary[letterObj.letter]" :key="item.word">
             <h3>{{ item.word }}</h3>
             <p><strong>Category:</strong> {{ item.category }}</p>
             <p><strong>Definition:</strong> {{ item.definition }}</p>
           </li>
         </ul>
-      </template>
+      </div>
     </div>
-    </div>
-  </template>
-  
-  <script>
-  import glossaryJSON from "../static/glossary.json";
-  
-  export default {
-    data() {
-      return {
-        glossary: glossaryJSON,
-      };
-    },
-    computed: {
-      sortedGlossary() {
-        // Create an object to store groups of glossary items by letter
-        let grouped = {};
-        
-        // Loop through glossary items and group them by the first letter of 'word'
-        this.glossary.forEach(item => {
-          let letter = item.word.charAt(0).toUpperCase(); // Get the first letter and convert to uppercase
-          if (!grouped[letter]) {
-            grouped[letter] = [];
-          }
-          grouped[letter].push(item);
+  </div>
+</template>
+
+<script>
+import glossaryJSON from "../static/glossary.json";
+
+export default {
+  data() {
+    return {
+      glossary: glossaryJSON,
+      letters: [] // Array to track letters and their expanded states
+    };
+  },
+  computed: {
+    sortedGlossary() {
+      // Create an object to store groups of glossary items by letter
+      let grouped = {};
+      
+      // Loop through glossary items and group them by the first letter of 'word'
+      this.glossary.forEach(item => {
+        let letter = item.word.charAt(0).toUpperCase(); // Get the first letter and convert to uppercase
+        if (!grouped[letter]) {
+          grouped[letter] = [];
+        }
+        grouped[letter].push(item);
+      });
+      
+      // Sort groups by letter
+      const sortedGroups = {};
+      Object.keys(grouped).sort().forEach(key => {
+        sortedGroups[key] = grouped[key].sort((a, b) => {
+          return a.word.localeCompare(b.word);
         });
-        
-        // Sort groups by letter
-        const sortedGroups = {};
-        Object.keys(grouped).sort().forEach(key => {
-          sortedGroups[key] = grouped[key].sort((a, b) => {
-            return a.word.localeCompare(b.word);
-          });
-        });
-        
-        return sortedGroups;
-      },
-      sortedLetters() {
-        // Return an array of sorted letters from sortedGlossary object keys
-        return Object.keys(this.sortedGlossary).sort();
+      });
+      
+      return sortedGroups;
+    }
+  },
+  mounted() {
+    // Initialize letters array with the expanded state set to false
+    this.letters = Object.keys(this.sortedGlossary).sort().map(letter => {
+      return { letter, expanded: false };
+    });
+  },
+  methods: {
+    toggleLetter(letter) {
+      const letterObj = this.letters.find(l => l.letter === letter);
+      if (letterObj) {
+        letterObj.expanded = !letterObj.expanded;
       }
-    },
-  };
-  </script>
-  
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add styling for the expand/collapse indicator */
+h2 span {
+  float: right;
+}
+</style>
