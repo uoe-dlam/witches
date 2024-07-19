4187c7f83484a9a3031e48a029a887e1afb45005
Ruby Imrie <s2056921@ed.ac.uk>
2024-07-19 11:20:51 +0100

    adding filter functionality

--- pages/memorials.vue
+++ pages/memorials.vue
@@ -10,44 +10,57 @@
           <div class="w-full h-full flex" v-if="filtersBox">
             <div class="h-full flex flex-col bg-white
                         rounded-tr-xl rounded-br-xl filters-shadow
-                        overflow-y-visible overflow-x-hidden
-                        relative" 
+                        overflow-y-auto overflow-x-hidden
+                        relative"
              style="width:90%">
-            <!-- Header -->
-            <div class="flex w-full flex-col bg-white md:sticky md:top-0 md:z-10" ref="FiltersHeader">
-              <div class="flex flex-col w-full h-full" style="backdrop-filter: blur(1.5px);">
-                <!-- Title and info-->
-                <div class="flex text-center mt-1">
-                  <h1 class="text-3xl mx-0 px-2">Map of Memorials</h1>
-                </div>
-
-                <!-- Display number of active witches. -->
-                <div class="ml-3 flex mt-3 items-center pb-2">
-                  <p class="mr-2 text-lg witchy-text">Showing</p>
-                  <div class="h-6 px-1 flex items-center justify-center mr-2 border-2 rounded-md text-white font-medium bg-slate-500 border-slate-700">
-                    <p>110</p>
+              <!-- Header -->
+              <div class="flex w-full flex-col bg-white md:sticky md:top-0 md:z-10" ref="FiltersHeader">
+                <div class="flex flex-col w-full h-full" style="backdrop-filter: blur(1.5px);">
+                  <!-- Title and info-->
+                  <div class="flex text-center mt-1">
+                    <h1 class="text-3xl mx-0 px-2">Map of Memorials</h1>
                   </div>
-                  <p class="mr-1 text-lg witchy-text">Locations</p>
+
+                  <!-- Display number of active witches. -->
+                  <div class="ml-3 flex mt-3 items-center pb-2">
+                    <p class="mr-2 text-lg witchy-text">Showing</p>
+                    <div class="h-6 px-1 flex items-center justify-center mr-2 border-2 rounded-md text-white font-medium bg-slate-500 border-slate-700">
+                      <p>{{ filteredMarkers.length }}</p>
+                    </div>
+                    <p class="mr-1 text-lg witchy-text">Locations</p>
+                  </div>
+                </div>
+                <div class="w-full border mt-1"></div>
+              </div>
+
+              <!-- Explanation Paragraph -->
+              <div class="ml-3 mb-3">
+                <p>This is a map showing locations relating to the Scottish witch trials. Some are memorials to remember them, some are tourist attractions to educate, and some are folklorian.</p>
+              </div>
+
+              <!-- Filter Checklist -->
+              <div class="ml-3 mb-3">
+                <h2 class="text-lg font-semibold">Filter by:</h2>
+                <div class="flex items-center">
+                  <input type="checkbox" id="poi-filter" v-model="filters.poi" class="mr-2" />
+                  <label for="poi-filter">Points of Interest</label>
+                </div>
+                <div class="flex items-center">
+                  <input type="checkbox" id="memorial-filter" v-model="filters.memorial" class="mr-2" />
+                  <label for="memorial-filter">Memorials</label>
                 </div>
               </div>
-              <div class="w-full border mt-1"></div>
             </div>
-            <!-- Explanation Paragraph -->
-            <div class="ml-3 mb-3">
-              <p>This is a map showing locations relating to the Scottish witch trials. Some are memorials to remember them, some are tourist attractions to educate, and some are folklorian</p>
-            </div>
-            <!-- Filter -->
-          </div>
-          <!-- Left chevron to hide filters. -->
-          <div class="w-8 flex flex-col justify-center ml-1" v-if="filtersBox" >
-            <div
-              class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
-              @click="toggleFiltersBox"
-            >
-              <img class="max-w-full max-h-full" src="/images/chevrons-left.svg" />
+            <!-- Left chevron to hide filters. -->
+            <div class="w-8 flex flex-col justify-center ml-1" v-if="filtersBox">
+              <div
+                class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
+                @click="toggleFiltersBox"
+              >
+                <img class="max-w-full max-h-full" src="/images/chevrons-left.svg" />
+              </div>
             </div>
           </div>
-        </div>
         </transition>
 
         <!-- Right chevron to show filters. -->
@@ -60,44 +73,44 @@
           </div>
         </div>
       </div>
-    
 
-    <!-- Map -->
-    <l-map class="w-full h-full z-0 absolute" :zoom="zoom" :center="center" ref="myMap">
-      <l-control-zoom position="bottomright"></l-control-zoom>
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker v-for="(memorial, i) in markers" :key="i" :lat-lng="memorial.longLat">
-        <l-popup class="adapted-popup">
-          <h3>{{ memorial.name }}</h3><br />
-          <div v-if="memorial.imageUrl" class="mb-5 flex justify-center items-center">
-            <a :title="'Image Source: ' + memorial.imageUrl" :href="memorial.imageUrl">
-              <img width="150vw" :alt="memorial.name" :src="memorial.imageUrl" />
-            </a>
-          </div>
-          <div>
-            <b>Instance Of:</b> {{ memorial.instance }}<br />
-            <b>Description:</b> {{ memorial.description }}<br />
-            <b>Location:</b> {{ memorial.location }}<br />
-            <b>Street Address:</b> {{ memorial.streetAddress }}<br />
-            <a :href="memorial.url" class="underline text-sky-500 hover:text-sky-700">Read More Here</a><br />
-          </div>
-        </l-popup>
-        <l-icon :icon-anchor="iconAnchor">
-          <div class="icon-wrapper">
-            <div v-if="poiList.includes(memorial.instance)">
-              <img src="../static/images/witch-single-brown.png" class="zoomed-in-img" />
+      <!-- Map -->
+      <l-map class="w-full h-full z-0 absolute" :zoom="zoom" :center="center" ref="myMap">
+        <l-control-zoom position="bottomright"></l-control-zoom>
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        <l-marker v-for="(memorial, i) in filteredMarkers" :key="i" :lat-lng="memorial.longLat">
+          <l-popup class="adapted-popup">
+            <h3>{{ memorial.name }}</h3><br />
+            <div v-if="memorial.imageUrl" class="mb-5 flex justify-center items-center">
+              <a :title="'Image Source: ' + memorial.imageUrl" :href="memorial.imageUrl">
+                <img width="150vw" :alt="memorial.name" :src="memorial.imageUrl" />
+              </a>
             </div>
-            <div v-else>
-              <img src="../static/images/witch-single-grey.png" class="zoomed-in-img" />
+            <div>
+              <b>Instance Of:</b> {{ memorial.instance }}<br />
+              <b>Description:</b> {{ memorial.description }}<br />
+              <b>Location:</b> {{ memorial.location }}<br />
+              <b>Street Address:</b> {{ memorial.streetAddress }}<br />
+              <a :href="memorial.url" class="underline text-sky-500 hover:text-sky-700">Read More Here</a><br />
             </div>
-            <img class="icon-shadow" src="../static/images/witch-single-shadow.png" />
-          </div>
-        </l-icon>
-      </l-marker>
-    </l-map>
-  </div>
+          </l-popup>
+          <l-icon :icon-anchor="iconAnchor">
+            <div class="icon-wrapper">
+              <div v-if="poiList.includes(memorial.instance)">
+                <img src="../static/images/witch-single-brown.png" class="zoomed-in-img" />
+              </div>
+              <div v-else>
+                <img src="../static/images/witch-single-grey.png" class="zoomed-in-img" />
+              </div>
+              <img class="icon-shadow" src="../static/images/witch-single-shadow.png" />
+            </div>
+          </l-icon>
+        </l-marker>
+      </l-map>
+    </div>
   </div>
 </template>
+
 
 <script>
 import { SPARQLQueryDispatcher } from '~/assets/js/SPARQLQueryDispatcher'
@@ -115,8 +128,25 @@ export default {
       sparqlUrl: 'https://query.wikidata.org/sparql',
       poiList: ['historic landmark', 'tourist attraction', 'statue'],
       memList: ['memorial', 'standing stone', 'commemorative plaque'],
-      descriptions: {} // To store descriptions from JSON
+      descriptions: {}, 
+      filters: {
+        poi: true,
+        memorial: true,
+      },
     }
+  },
+  computed: {
+    filteredMarkers() {
+      return this.markers.filter(marker => {
+        if (this.filters.poi && this.poiList.includes(marker.instance)) {
+          return true;
+        }
+        if (this.filters.memorial && this.memList.includes(marker.instance)) {
+          return true;
+        }
+        return false;
+      });
+    },
   },
   methods: {
     async loadDescriptions() {
@@ -154,18 +184,18 @@ export default {
           let imageUrl = item.hasOwnProperty('image') ? item.image.value : '';
           let streetAddress = item.hasOwnProperty('address') ? item.address.value : '';
           let url = item.hasOwnProperty('url') ? item.url.value : '';
-          let description = this.descriptions[id.split('/').pop()] || ''; // Use description from JSON
+          let description = this.descriptions[id.split('/').pop()] || ''; 
 
           let memorial = {
             id: id,
             name: item.itemLabel.value,
             longLat: memorialCoords,
             location: memorialLocation,
-            instanceOf: instance,
+            instance: instance,
             imageUrl: imageUrl,
             streetAddress: streetAddress,
             url: url,
-            description: description // Add description to the memorial object
+            description: description 
           }
 
           memorials.push(memorial);
@@ -193,7 +223,7 @@ export default {
             streetAddress: streetAddress,
             url: url,
             memorials: [memorial],
-            description: description // Add description to the marker
+            description: description 
           }
 
           this.markers.push(marker);
@@ -230,7 +260,7 @@ export default {
   },
 
   async mounted() {
-    await this.loadDescriptions(); // Load descriptions first
+    await this.loadDescriptions(); 
     this.loadMemorials();
     console.log('Markers:', this.markers);
   },
@@ -241,17 +271,3 @@ export default {
 };
 </script>
 
-<style>
-.zoomed-in-img {
-   float: left;
-   width: 25px;
-   height: 38px;
-}
-
-.icon-shadow {
-    position: absolute;
-    top: 15px !important;
-    left: 0;
-    z-index: -1;
-}
-</style>
