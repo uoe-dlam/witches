7e03e7c4bf552c2c6ba29a79422a4cf27caec465
Ruby Imrie <s2056921@ed.ac.uk>
2024-09-13 11:20:00 +0100

    getting scroll to work

--- components/CategoryGlossary.vue
+++ components/CategoryGlossary.vue
@@ -14,7 +14,7 @@
           <span v-if="categoryObj.expanded">-</span><span v-else>+</span>
         </h2>
         <hr class="mb-3">
-        <ul v-show="categoryObj.expanded" class="mb-3">
+        <ul v-show="categoryObj.expanded" class="mb-3 ml-4">
           <li v-for="item in sortedGlossary[categoryObj.category]" :key="item.word" class="mb-3">
             <h3 class="font-semibold">{{ item.word }}</h3>
             <p><span class="font-medium">Definition:</span> {{ item.definition }}</p>
@@ -46,10 +46,8 @@ export default {
   },
   computed: {
     sortedGlossary() {
-      // Object to store groups of glossary items by category
       let grouped = {};
 
-      // Group by category
       this.glossary.forEach(item => {
         let category = item.category || 'Uncategorized';
         if (!grouped[category]) {
@@ -58,7 +56,6 @@ export default {
         grouped[category].push(item);
       });
 
-      // Sort categories and items within each category alphabetically
       const sortedGroups = {};
       Object.keys(grouped).sort().forEach(key => {
         sortedGroups[key] = grouped[key].sort((a, b) => a.word.localeCompare(b.word));
@@ -68,29 +65,43 @@ export default {
     }
   },
   mounted() {
-    // Create categories array for expandable sections
     this.categories = Object.keys(this.sortedGlossary).sort().map(category => {
       return { category, expanded: false };
     });
 
-    // Get category from query param or prop
-    const queryCategory = this.initialCategory || this.$route.query.category;
-    if (queryCategory) {
-      const categoryObj = this.categories.find(c => c.category === queryCategory);
-      if (categoryObj) {
-        categoryObj.expanded = true;
-
-        // Scroll to the category header after DOM updates
-        this.$nextTick(() => {
-          const categoryHeader = this.$refs[`category-${queryCategory}`];
-          if (categoryHeader && categoryHeader[0]) {
-            categoryHeader[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
-          }
-        });
-      }
+    this.handleCategoryFromQuery();
+  },
+  watch: {
+    '$route.query.category': {
+      handler() {
+        // to add a slight delay to ensure the DOM and route updates are complete -nextTick alone wasn't working
+        setTimeout(() => {
+          this.handleCategoryFromQuery();
+        }, 300); // 300ms
+      },
+      immediate: true
     }
   },
   methods: {
+    handleCategoryFromQuery() {
+      const queryCategory = this.initialCategory || this.$route.query.category;
+      if (queryCategory) {
+        const categoryObj = this.categories.find(c => c.category === queryCategory);
+        if (categoryObj) {
+          categoryObj.expanded = true;
+
+          // Scroll after the DOM has updated
+          this.$nextTick(() => {
+            setTimeout(() => {
+              const categoryHeader = this.$refs[`category-${queryCategory}`];
+              if (categoryHeader && categoryHeader[0]) {
+                categoryHeader[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
+              }
+            }, 100); // Small delay before scrolling
+          });
+        }
+      }
+    },
     toggleShowingCategory(category) {
       const categoryObj = this.categories.find(c => c.category === category);
       if (categoryObj) {
@@ -110,6 +121,9 @@ export default {
   }
 };
 </script>
+
+
+
 
 
   
