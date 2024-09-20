e2fc098d617fc5f7ebe681e067468f2a47ea2226
Ruby Imrie <s2056921@ed.ac.uk>
2024-09-20 10:56:13 +0100

    nuxt-link

--- components/MainNavBarItem.vue
+++ components/MainNavBarItem.vue
@@ -3,9 +3,9 @@
     <a :href="url" class="block mt-4 xl:pt-1 xl:pb-1 xl:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" v-if="external">
       <slot></slot>
     </a>
-    <nuxt-link :to="url" class="block xl:pt-1 xl:pb-1 mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" :class="{ 'menuBarItemActive' :  isSubpageActive(subpages)}" v-else>
+    <NuxtLink :to="url" class="block xl:pt-1 xl:pb-1 mt-4 lg:inline-block lg:mt-0 text-gray-500 hover:text-black lg:mr-2 xl:mr-4" :class="{ 'menuBarItemActive' :  isSubpageActive(subpages)}" v-else>
       <slot></slot>
-    </nuxt-link>
+    </NuxtLink>
     <div v-if="subpages.length > 0" class="block dropdown-content lg:absolute lg:hidden lg:bg-gray-200 lg:shadow-xl text-gray-500">
       <a v-for="(page, index) in subpages" :href="page.url" class="block pt-3 pb-0 pl-2 pr-2 lg:p-2 hover:text-black" :class="{ 'subMenuItemActive' : isSubActive(page.url)}">{{ page.name }}</a>
     </div>
