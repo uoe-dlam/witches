1fb475e9df030ebb36dd0a9280947f3375b65ac5
Ruby Imrie <s2056921@ed.ac.uk>
2024-09-30 11:57:30 +0100

    prettier on myFetch

--- composables/myFetch.ts
+++ composables/myFetch.ts
@@ -1,18 +1,17 @@
 export const myFetch = async (request, opts = {}) => {
-    const config = useRuntimeConfig();
-    const url = new URL(request, config.public.baseURL);
+  const config = useRuntimeConfig();
+  const url = new URL(request, config.public.baseURL);
 
-    try {
-      const data = await $fetch(url, opts)
-      if (!data ) {
-        throw createError({
-          statusMessage: `Could not fetch data.`,
-        });
-      }
-      return data;
-  
-    } catch (error) {
-      console.error("Fetch error");
-      throw error;
-    }  
-  };
+  try {
+    const data = await $fetch(url, opts);
+    if (!data) {
+      throw createError({
+        statusMessage: `Could not fetch data.`,
+      });
+    }
+    return data;
+  } catch (error) {
+    console.error("Fetch error");
+    throw error;
+  }
+};
