32daf4a81ceef3f67b5958891a1303f7334dc84a
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-03 12:24:23 +0100

    updating caption

--- pages/trial-procedures.vue
+++ pages/trial-procedures.vue
@@ -18,9 +18,9 @@
       Convictions were common but not universal; some people were acquitted.
     </p>
     <div>
-      <div class="lg:float-right lg:w-1/2 w-3/4 mx-auto mr-4">
-        <img src="../public/images/trial.png" class="w-full lg:ml-6" />
-        <p class="text-xs text-center">Map of Accused Witches. CC-BY-SA 4.0</p>
+      <div class="lg:float-right lg:w-1/2 lg:ml-6 w-3/4 mx-auto mr-4">
+        <img src="../public/images/trial.png" class="w-full " />
+        <p class="text-xs text-center mx-auto">Figure 1. Suspected witches from North Berwick appear before James VI and two Scottish magistrates, from the English pamphlet Newes from Scotland (1591) (Public domain, via Wikimedia Commons)</p>
       </div>
       <p class="mb-3 text-justify">
         Evidence against the accused person had to be gathered beforehand. This
