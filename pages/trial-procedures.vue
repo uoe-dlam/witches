47d8556077bd8ccc1db650a80e88e9c73ef3fd82
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-03 12:21:10 +0100

    adding trial precedures page

---
+++ pages/trial-procedures.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="main-content" class="text-wrapper w-full">
+    <h1 class="text-center text-2xl md:text-3xl lg:text-4xl mb-6">
+      Witchcraft trial procedure and evidence in Scotland
+    </h1>
+
+    <p class="mb-3 text-justify">
+      Accused witches were tried in criminal courts. In the courtroom on the day
+      of the trial, there was a judge who presided over the proceedings and made
+      rulings on the law, a jury (fifteen property owning members of the local
+      community) who decided on a verdict of innocent or guilty, and a clerk to
+      keep records. There would also be a prosecutor to present the accusations.
+      The accused person would be able to speak in their own defence. If the
+      trial was held in the central Edinburgh court of justiciary, the court
+      would also assign a defence lawyer to the person. If the jury convicted
+      the accused person, the judge would pass sentence. In a witchcraft case
+      the sentence was usually death by strangulation followed by burning.
+      Convictions were common but not universal; some people were acquitted.
+    </p>
+    <div>
+      <div class="lg:float-right lg:w-1/2 w-3/4 mx-auto mr-4">
+        <img src="../public/images/trial.png" class="w-full lg:ml-6" />
+        <p class="text-xs text-center">Map of Accused Witches. CC-BY-SA 4.0</p>
+      </div>
+      <p class="mb-3 text-justify">
+        Evidence against the accused person had to be gathered beforehand. This
+        was often done by local church courts - kirk sessions or presbyteries.
+        These bodies were not criminal courts, but they could summon people and
+        interrogate them. Two main kinds of evidence were used against
+        witchcraft suspects: neighbours' testimony, and the suspect's own
+        confession. If the suspect was thought to have bewitched her or his
+        neighbours, these neighbours could make statements about this, telling
+        stories about quarrels and bewitchments that had followed. Or, if the
+        suspect was thought to have made a pact with the Devil, the suspect
+        herself or himself could be interrogated about this. Interrogations were
+        often done coercively; usually little was written down about this, but
+        the most common form of torture seems to have been sleep deprivation.
+        This was used due to its effectiveness and was usually conducted by
+        local authorities. Confession evidence tended to have much more about
+        the suspect's dealings with the Devil. Confessions could also implicate
+        other suspects, if a suspect was asked something like 'Who else was
+        present when you met the Devil?'.
+      </p>
+    </div>
+    <p class="mb-3 text-justify">
+      A third kind of evidence was sometimes used: 'pricking' to discover the
+      Devil's mark. The accused witch was stripped partially or completely
+      naked, and their body was pricked with a pin to find an insensitive spot
+      where the pin could be inserted. The idea was that the Devil placed an
+      invisible mark on the witch's body at the time when they agreed to become
+      a witch, in an echo or parody of Christian baptism; the pin would find
+      this mark.
+    </p>
+    <p class="mb-3 text-justify">
+      It is important to distinguish witch-pricking from 'torture'. Torture
+      inflicted pain in order to make the suspect confess; the aim was to obtain
+      a confession as evidence. This was not a punishment, but an attempt to
+      secure evidence. Witch-pricking aimed to inflict no pain; the aim was to
+      identify the Devil's mark as evidence. However, in practice the process
+      probably did cause at least some pain. It was also coercive and
+      humiliating for a person, especially a woman, to be stripped naked in the
+      presence of a group of men. One side-effect of pricking could be to break
+      down a suspect's resistance to interrogation; some suspects began to
+      confess after they were told that a mark had been found
+    </p>
+    <p class="mb-3 text-justify">
+      It is important to remember that, whilst this map allows you to explore
+      the accused witches for who names can be found, there are many more not
+      included on this map for whom the evidence is too sparse, or unfortunately
+      just does not exist. If you are interested in learning more about the
+      witch-hunts in Scotland, there are various books and articles that we can
+      recommend. Our Further Reading page gives you a list of up-to-date work on
+      witchcraft and witch-hunting in Scotland, for an older but more detailed
+      list of works, see the Survey of Scottish Witchcraft’s
+    </p>
+    <p class="mb-3 text-justify">
+      Witch-prickers were all men; sometimes an ordinary interrogator (such as a
+      parish elder or minister) would do the pricking, but during the major
+      panics a handful of professional 'prickers' claimed special expertise in
+      the technique, and travelled round pricking numerous suspects in return
+      for modest fees. Most prickers seem to have believed in the genuineness of
+      their own powers, though there was a notorious fraudulent episode in
+      Peebles in 1649-50 in which the pricker George Cathie colluded with the
+      accused witch Janet Coutts to get her to name other suspects whom he would
+      then prick. Several dozen executions occurred before the fraud was
+      detected. Another pricker, 'Mr Paterson', turned out to be a woman in
+      disguise, named Christian Cadell
+    </p>
+    <p class="mb-3 text-justify">
+      It should be noted that Scottish courts hardly ever used the 'swimming
+      test' or 'water ordeal' in which suspects were dropped in water; this was
+      recorded in Scotland only in 1597. The swimming test was used from time to
+      time in some other European countries, though it was never normal. (Today
+      there is a belief that innocent suspects sank and were drowned; this is a
+      myth. Nobody drowned; suspects had a rope tied to them and were pulled
+      out.)
+    </p>
+    <p class="mb-3 text-justify">
+      The use of these techniques in interrogations declined in Scotland towards
+      the end of the 17th century. This is due to a growth in judicial
+      scepticism, particularly surrounding the use of torture. Thus, the use of
+      torture in interrogations of accused witches declined from the 1660s; yet
+      we know that it still used in a couple of cases until 1708.
+    </p>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: "basic",
+});
+
+export default {
+  data: () => ({}),
+  methods: {},
+  computed: {},
+  mounted: function () {},
+};
+</script>
+
+<style scoped></style>
