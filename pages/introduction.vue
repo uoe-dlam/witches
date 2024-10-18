fd4b08c2617ab50259c0019f1db7dc2385f12e86
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-18 15:09:31 +0100

    moving to new faq page

--- pages/introduction.vue
+++ pages/introduction.vue
@@ -112,36 +112,6 @@
         >‘Further Reading’ </a
       >list.
     </p>
-    <h2 class="text-center mb-6">FAQ with Professor Julian Goodare</h2>
-    <p class="mb-6 text-justify">
-      University of Edinburgh students, Ruby Imrie and Ellie Whitehead, sat down
-      with Professor Julian Goodare, lecturer in Scottish History, to ask him
-      some questions about the Scottish witch trials. Here are his answers in
-      the video playlist below. For more FAQ, please refer to the Survey of
-      Scottish Witchcraft page
-      <a
-        class="underline hover:text-gray"
-        href="https://witches.hca.ed.ac.uk/faq/"
-        >here</a
-      >
-      or contact us using our
-      <a class="underline hover:text-gray" href="/contact">contact form.</a>
-    </p>
-    <div class="flex justify-center">
-      <iframe
-        src="https://media.ed.ac.uk/embedplaylist/secure/embed/v2/0/playlistId/1_3fijkxq7/uiConfId/55171522"
-        width="800px"
-        height="450px"
-        allowfullscreen
-        webkitallowfullscreen
-        mozAllowFullScreen
-        allow="autoplay *; fullscreen *; encrypted-media *"
-        referrerpolicy="no-referrer-when-downgrade"
-        sandbox="allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
-        frameborder="0"
-        title="Kaltura Player"
-      ></iframe>
-    </div>
   </div>
 </template>
 
