53e445d79e3d538b6e03627eb53e23dfa18c1b03
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-19 04:27:03 +0100

    adding loading message

--- pages/memorials.vue
+++ pages/memorials.vue
@@ -1,240 +1,242 @@
 <template>
   <div id="map-wrapper" class="flex flex-col h-full w-full relative">
-    <!-- Filters -->
-    <div class="absolute flex flex-col w-full h-full">
-      <div
-        class="xs:w-11/12 sm:w-1/2 lg:w-2/5 xl:w-1/3 z-20 left-0"
-        :style="filtersBox ? { height: '97%' } : { 'pointer-events': 'none' }"
-      >
-        <transition>
-          <div class="w-full h-full flex" v-if="filtersBox">
-            <div
-              class="h-full flex flex-col bg-white rounded-tr-xl rounded-br-xl filters-shadow overflow-y-auto overflow-x-hidden relative"
-              style="width: 90%"
-            >
-              <!-- Header -->
+    <loading-message v-if="loading" />
+    <div v-else>
+      <!-- Filters -->
+      <div class="absolute flex flex-col w-full h-full">
+        <div
+          class="xs:w-11/12 sm:w-1/2 lg:w-2/5 xl:w-1/3 z-20 left-0"
+          :style="filtersBox ? { height: '97%' } : { 'pointer-events': 'none' }"
+        >
+          <transition>
+            <div class="w-full h-full flex" v-if="filtersBox">
               <div
-                class="flex w-full flex-col bg-white md:sticky md:top-0 md:z-10"
-                ref="FiltersHeader"
+                class="h-full flex flex-col bg-white rounded-tr-xl rounded-br-xl filters-shadow overflow-y-auto overflow-x-hidden relative"
+                style="width: 90%"
+              >
+                <!-- Header -->
+                <div
+                  class="flex w-full flex-col bg-white md:sticky md:top-0 md:z-10"
+                  ref="FiltersHeader"
+                >
+                  <div
+                    class="flex flex-col w-full h-full"
+                    style="backdrop-filter: blur(1.5px)"
+                  >
+                    <!-- Title and info-->
+                    <div class="flex text-center mt-1">
+                      <h3 class="mx-0 px-2">
+                        Map of Memorials & Sites of Interest
+                      </h3>
+                    </div>
+
+                    <!-- Display number of active witches. -->
+                    <div class="ml-3 flex mt-3 items-center pb-2">
+                      <p class="mr-2 text-lg witchy-text">Showing</p>
+                      <div
+                        class="h-6 px-1 flex items-center justify-center mr-2 border-2 rounded-md text-white font-medium bg-slate-500 border-slate-700"
+                      >
+                        <p>{{ filteredMarkers.length }}</p>
+                      </div>
+                      <p class="mr-1 text-lg witchy-text">Locations</p>
+                    </div>
+                  </div>
+                  <div class="w-full border mt-1"></div>
+                </div>
+
+                <!-- Explanation Paragraph -->
+                <div class="m-3">
+                  <p class="mb-3">
+                    This map displays some of the locations across Scotland
+                    connected to the memory of the Scottish Witch trials
+                    (1563-1736). It includes memorials honouring those accused
+                    of witchcraft during this period, as well as highlighting
+                    tourist attractions and sites connected to popular belief
+                    surrounding witchcraft. These memorials are seperate to the
+                    witch hunts, appearing first in the 18th century and
+                    continuing to appear right up to present day. The mzp
+                    documents witchcraft memorialisation in Scotland and does
+                    not endorse specific sites.
+                  </p>
+                  <p>
+                    If you know of any other memorials or sites of interest
+                    related to the Scottish witch trials that are not already
+                    included, please let us know on our
+                    <a class="underline hover:text-gray-500" href="/contact"
+                      >Contact</a
+                    >
+                    page.
+                  </p>
+                </div>
+
+                <!-- Filter Checklist -->
+                <div class="ml-3 mb-6">
+                  <h4 class="mb-3">Filters</h4>
+                  <div class="flex space-x-6">
+                    <div class="flex flex-col items-center">
+                      <label
+                        for="memorial-filter"
+                        class="flex flex-col items-center cursor-pointer"
+                      >
+                        <img
+                          class="w-8 mb-2"
+                          :src="memIcon"
+                          alt="Memorial Icon"
+                        />
+                        <span class="text-xs">Memorials</span>
+                      </label>
+                      <input
+                        type="checkbox"
+                        id="memorial-filter"
+                        v-model="filters.memorial"
+                      />
+                    </div>
+                    <div class="flex flex-col items-center">
+                      <label
+                        for="poi-filter"
+                        class="flex flex-col items-center cursor-pointer"
+                      >
+                        <img
+                          class="w-8 mb-2"
+                          :src="poiIcon"
+                          alt="Point of Interest Icon"
+                        />
+                        <span class="text-xs">Sites of Interest</span>
+                      </label>
+                      <input
+                        type="checkbox"
+                        id="poi-filter"
+                        v-model="filters.poi"
+                      />
+                    </div>
+                    <div class="flex flex-col items-center">
+                      <label
+                        for="poi-filter"
+                        class="flex flex-col items-center cursor-pointer"
+                      >
+                        <img
+                          class="w-8 mb-2"
+                          :src="touristIcon"
+                          alt="Point of Interest Icon"
+                        />
+                        <span class="text-xs">Tourist Attraction</span>
+                      </label>
+                      <input
+                        type="checkbox"
+                        id="poi-filter"
+                        v-model="filters.tourist"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Left chevron to hide filters. -->
+              <div
+                class="w-8 flex flex-col justify-center ml-1"
+                v-if="filtersBox"
               >
                 <div
-                  class="flex flex-col w-full h-full"
-                  style="backdrop-filter: blur(1.5px)"
+                  class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
+                  @click="toggleFiltersBox"
                 >
-                  <!-- Title and info-->
-                  <div class="flex text-center mt-1">
-                    <h3 class="mx-0 px-2">
-                      Map of Memorials & Sites of Interest
-                    </h3>
-                  </div>
-
-                  <!-- Display number of active witches. -->
-                  <div class="ml-3 flex mt-3 items-center pb-2">
-                    <p class="mr-2 text-lg witchy-text">Showing</p>
-                    <div
-                      class="h-6 px-1 flex items-center justify-center mr-2 border-2 rounded-md text-white font-medium bg-slate-500 border-slate-700"
-                    >
-                      <p>{{ filteredMarkers.length }}</p>
-                    </div>
-                    <p class="mr-1 text-lg witchy-text">Locations</p>
-                  </div>
-                </div>
-                <div class="w-full border mt-1"></div>
-              </div>
-
-              <!-- Explanation Paragraph -->
-              <div class="m-3">
-                <p class="mb-3">
-                  This map displays some of the locations across Scotland
-                  connected to the memory of the Scottish Witch trials
-                  (1563-1736). It includes memorials honouring those accused of
-                  witchcraft during this period, as well as highlighting tourist
-                  attractions and sites connected to popular belief surrounding
-                  witchcraft. These memorials are seperate to the witch hunts,
-                  appearing first in the 18th century and continuing to appear
-                  right up to present day. The mzp documents witchcraft
-                  memorialisation in Scotland and does not endorse specific
-                  sites.
-                </p>
-                <p>
-                  If you know of any other memorials or sites of interest
-                  related to the Scottish witch trials that are not already
-                  included, please let us know on our
-                  <a class="underline hover:text-gray-500" href="/contact"
-                    >Contact</a
-                  >
-                  page.
-                </p>
-              </div>
-
-              <!-- Filter Checklist -->
-              <div class="ml-3 mb-6">
-                <h4 class="mb-3">Filters</h4>
-                <div class="flex space-x-6">
-                  <div class="flex flex-col items-center">
-                    <label
-                      for="memorial-filter"
-                      class="flex flex-col items-center cursor-pointer"
-                    >
-                      <img
-                        class="w-8 mb-2"
-                        :src="memIcon"
-                        alt="Memorial Icon"
-                      />
-                      <span class="text-xs">Memorials</span>
-                    </label>
-                    <input
-                      type="checkbox"
-                      id="memorial-filter"
-                      v-model="filters.memorial"
-                    />
-                  </div>
-                  <div class="flex flex-col items-center">
-                    <label
-                      for="poi-filter"
-                      class="flex flex-col items-center cursor-pointer"
-                    >
-                      <img
-                        class="w-8 mb-2"
-                        :src="poiIcon"
-                        alt="Point of Interest Icon"
-                      />
-                      <span class="text-xs">Sites of Interest</span>
-                    </label>
-                    <input
-                      type="checkbox"
-                      id="poi-filter"
-                      v-model="filters.poi"
-                    />
-                  </div>
-                  <div class="flex flex-col items-center">
-                    <label
-                      for="poi-filter"
-                      class="flex flex-col items-center cursor-pointer"
-                    >
-                      <img
-                        class="w-8 mb-2"
-                        :src="touristIcon"
-                        alt="Point of Interest Icon"
-                      />
-                      <span class="text-xs">Tourist Attraction</span>
-                    </label>
-                    <input
-                      type="checkbox"
-                      id="poi-filter"
-                      v-model="filters.tourist"
-                    />
-                  </div>
+                  <img
+                    class="max-w-full max-h-full"
+                    src="/images/chevrons-left.svg"
+                  />
                 </div>
               </div>
             </div>
-            <!-- Left chevron to hide filters. -->
-            <div
-              class="w-8 flex flex-col justify-center ml-1"
-              v-if="filtersBox"
-            >
-              <div
-                class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
-                @click="toggleFiltersBox"
-              >
-                <img
-                  class="max-w-full max-h-full"
-                  src="/images/chevrons-left.svg"
-                />
-              </div>
-            </div>
-          </div>
-        </transition>
+          </transition>
 
-        <!-- Right chevron to show filters. -->
-        <div
-          class="w-8 flex flex-col justify-center ml-1 h-full"
-          v-if="!filtersBox"
-          style="pointer-events: auto"
-        >
+          <!-- Right chevron to show filters. -->
           <div
-            class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
-            @click="toggleFiltersBox"
+            class="w-8 flex flex-col justify-center ml-1 h-full"
+            v-if="!filtersBox"
+            style="pointer-events: auto"
           >
-            <img
-              class="max-w-full max-h-full"
-              src="/images/chevrons-right.svg"
-            />
+            <div
+              class="flex items-center justify-center w-8 h-8 rounded-full bg-slate-200 filters-shadow"
+              @click="toggleFiltersBox"
+            >
+              <img
+                class="max-w-full max-h-full"
+                src="/images/chevrons-right.svg"
+              />
+            </div>
           </div>
         </div>
-      </div>
-
-      <!-- Map -->
-      <LMap
-        class="w-full h-full z-0 absolute"
-        :zoom="zoom"
-        :center="center"
-        ref="myMap"
-      >
-        <LControlZoom position="bottomright"></LControlZoom>
-        <LTileLayer :url="url" :attribution="attribution"></LTileLayer>
-        <LMarker
-          v-for="(memorial, i) in filteredMarkers"
-          :key="i"
-          :lat-lng="memorial.longLat"
+        <!-- Map -->
+        <LMap
+          class="w-full h-full z-0 absolute"
+          :zoom="zoom"
+          :center="center"
+          ref="myMap"
         >
-          <LPopup class="adapted-popup">
-            <h3>{{ memorial.name }}</h3>
-            <br />
-            <div
-              v-if="memorial.imageUrl"
-              class="mb-5 flex justify-center items-center"
-            >
-              <a
-                :title="'Image Source: ' + memorial.imageUrl"
-                :href="memorial.imageUrl"
+          <LControlZoom position="bottomright"></LControlZoom>
+          <LTileLayer :url="url" :attribution="attribution"></LTileLayer>
+          <LMarker
+            v-for="(memorial, i) in filteredMarkers"
+            :key="i"
+            :lat-lng="memorial.longLat"
+          >
+            <LPopup class="adapted-popup">
+              <h3>{{ memorial.name }}</h3>
+              <br />
+              <div
+                v-if="memorial.imageUrl"
+                class="mb-5 flex justify-center items-center"
               >
-                <img
-                  width="150vw"
-                  :alt="memorial.name"
-                  :src="memorial.imageUrl"
-                />
-              </a>
-            </div>
-            <div>
-              <div v-if="memorial.instance">
-                <b>Instance Of:</b> {{ memorial.instance }}<br />
-              </div>
-              <div v-if="memorial.description">
-                <b>Description:</b> <span v-html="memorial.description"></span
-                ><br />
-              </div>
-              <div v-if="memorial.location">
-                <b>Location:</b> {{ memorial.location }}<br />
-              </div>
-              <div v-if="memorial.streetAddress">
-                <b>Street Address:</b> {{ memorial.streetAddress }}<br />
-              </div>
-              <div v-if="memorial.url">
                 <a
-                  :href="memorial.url"
-                  class="underline text-sky-500 hover:text-sky-700"
-                  >Read More Here</a
-                ><br />
+                  :title="'Image Source: ' + memorial.imageUrl"
+                  :href="memorial.imageUrl"
+                >
+                  <img
+                    width="150vw"
+                    :alt="memorial.name"
+                    :src="memorial.imageUrl"
+                  />
+                </a>
               </div>
-            </div>
-          </LPopup>
-          <LIcon :icon-anchor="iconAnchor" class-name="">
-            <div class="icon-wrapper">
-              <div v-if="memorial.type === 'memorial'">
-                <img :src="memIcon" class="zoomed-in-img" />
+              <div>
+                <div v-if="memorial.instance">
+                  <b>Instance Of:</b> {{ memorial.instance }}<br />
+                </div>
+                <div v-if="memorial.description">
+                  <b>Description:</b> <span v-html="memorial.description"></span
+                  ><br />
+                </div>
+                <div v-if="memorial.location">
+                  <b>Location:</b> {{ memorial.location }}<br />
+                </div>
+                <div v-if="memorial.streetAddress">
+                  <b>Street Address:</b> {{ memorial.streetAddress }}<br />
+                </div>
+                <div v-if="memorial.url">
+                  <a
+                    :href="memorial.url"
+                    class="underline text-sky-500 hover:text-sky-700"
+                    >Read More Here</a
+                  ><br />
+                </div>
               </div>
-              <div v-if="memorial.type === 'site of interest'">
-                <img :src="poiIcon" class="zoomed-in-img" />
+            </LPopup>
+            <LIcon :icon-anchor="iconAnchor" class-name="">
+              <div class="icon-wrapper">
+                <div v-if="memorial.type === 'memorial'">
+                  <img :src="memIcon" class="zoomed-in-img" />
+                </div>
+                <div v-if="memorial.type === 'site of interest'">
+                  <img :src="poiIcon" class="zoomed-in-img" />
+                </div>
+                <div v-if="memorial.type === 'tourist attraction'">
+                  <img :src="touristIcon" class="zoomed-in-img" />
+                </div>
+                <img class="icon-shadow" :src="shadow" />
               </div>
-              <div v-if="memorial.type === 'tourist attraction'">
-                <img :src="touristIcon" class="zoomed-in-img" />
-              </div>
-              <img class="icon-shadow" :src="shadow" />
-            </div>
-          </LIcon>
-        </LMarker>
-      </LMap>
+            </LIcon>
+          </LMarker>
+        </LMap>
+      </div>
     </div>
   </div>
 </template>
@@ -245,14 +247,17 @@ import memIcon from "../public/images/memorial-icon-red.png";
 import poiIcon from "../public/images/memorial-icon-blue.png";
 import touristIcon from "../public/images/memorial-icon-green.png";
 import shadow from "../public/images/witch-single-shadow.png";
+import LoadingMessage from "../components/LoadingMessage.vue";
 
 definePageMeta({
   layout: "default",
 });
 
 export default {
+  components: { LoadingMessage },
   data() {
     return {
+      loading: true,
       filtersBox: true,
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       zoom: 7,
