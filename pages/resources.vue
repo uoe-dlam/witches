710ef9de7734cf43670699e732c826d73f7738a2
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-20 17:17:16 +0100

    removing mb-3

--- pages/resources.vue
+++ pages/resources.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="main-content" class="text-wrapper">
     <h1 class="text-center mb-6">Resources</h1>
-    <p class="mb-3 link">
+    <p class="link">
       <a href="arcgis_online_files.zip" download
         >Download zipfile of CSV files for ArcGIS Online</a
       >
     </p>
-    <p class="mb-3 link">
+    <p class="link">
       <a
         href="https://docs.google.com/spreadsheets/d/14W8cWmqTaTgkwTFOjMYzV-BzqJgrGz8dKafmGfeuFPs/edit?usp=sharing"
         target="_blank"
         >Spreadsheet of unfound place-names</a
       >
     </p>
-    <p class="mb-3 link">
+    <p class="link">
       <a
         href="https://docs.google.com/document/d/14JcTa6twIjsx69ifoZxVf-VaXR7nFmmQhu52zXUYUD4/edit?usp=sharing"
         target="_blank"
         >Items added to Wikidata word document</a
       >
     </p>
-    <p class="mb-3 link">
+    <p class="link">
       <a href="https://w.wiki/6rX" target="_blank"
         >Query for accused witches residence map</a
       >
