311576c36017b51eedcd68648a77900ee10c2b4c
Ruby Imrie <s2056921@ed.ac.uk>
2024-10-22 11:31:52 +0100

    removing empty pop up

--- pages/named-witches.vue
+++ pages/named-witches.vue
@@ -3,16 +3,6 @@
     <div id="main-content" class="pl-5 pr-5 pt-3 pb-3">
       <div class="flex content-start items-center">
         <h1>Trial Mentions Network Graph</h1>
-        <div
-          class="inline-flex items-center justify-center align-middle rounded-full border-r-2 border-l-2 border-gray-400 w-6 h-6 hover:w-7 hover:h-7 mb-0.5 ml-1 cursor-pointer"
-        >
-          <img
-            src="/images/infoIcon.svg"
-            class="w-full h-full pt-0.5"
-            alt="Page Information Popup"
-            @click="showPageInfo()"
-          />
-        </div>
       </div>
     </div>
     <div style="height: 90%; min-height: 400px">
@@ -54,25 +44,12 @@
 </template>
 
 <script>
-import Swal from "sweetalert2";
-
 definePageMeta({
   layout: "default",
 });
 export default {
   data: () => ({}),
-  methods: {
-    showPageInfo() {
-      Swal.fire({
-        title: "Social Class Bubble Charts",
-        html: "<div>Histropedia Timeline</div>",
-        footer: "witches.is.ed.ac.uk",
-        confirmButtonText: "Close",
-        type: "info",
-        showCloseButton: true,
-      });
-    },
-  },
+  methods: {},
   computed: {},
   mounted: function () {},
 };
