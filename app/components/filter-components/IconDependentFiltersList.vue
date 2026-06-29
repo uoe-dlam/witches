76932ea2f6999cf432961c99951d0b9e8cc93b76
Leopold Ramutsamaya <leoramsy@gmail.com>
2026-06-29 13:49:37 +0100

    migrate IconDependentFiltersList component

--- app/components/filter-components/IconDependentFiltersList.vue
+++ app/components/filter-components/IconDependentFiltersList.vue
@@ -12,12 +12,12 @@
                 <div class="flex justify-center items-center">
                     <input
                         :checked="filterItem.active"
-                        @change="filterEmit(filterType)"
                         type="checkbox"
+                        @change="filterEmit(filterType)"
                     />
                     <img
-                        class="witch-icon mb-1 ml-0.5"
                         :src="filterItem.iconUrl"
+                        class="witch-icon mb-1 ml-0.5"
                     />
                 </div>
                 <p class="text-xs text-center mb-0">{{ filterItem.label }}</p>
@@ -32,8 +32,8 @@
                     <div class="flex mb-3 mx-2 w-full items-center">
                         <input
                             :checked="filterItem.active"
-                            @change="filterEmit(filterType)"
                             type="checkbox"
+                            @change="filterEmit(filterType)"
                         />
                         <p class="text-xs text-center ml-1 mb-0">
                             {{ filterItem.label }}
@@ -49,9 +49,9 @@
                         class="container flex items-center justify-center ml-1"
                     >
                         <input
-                            type="radio"
                             :checked="false"
                             name="radio"
+                            type="radio"
                             @change="emitSetPropertyToCurrent(property)"
                         />
                         <span class="checkmark"></span>
@@ -63,78 +63,72 @@
     </div>
 </template>
 
-<script>
-export default {
-    props: {
-        currentProperty: {
-            type: String,
-            required: true,
-        },
-        property: {
-            type: String,
-            required: true,
-        },
-        propertyLabel: {
-            type: String,
-            required: true,
-        },
-        filterTypes: {
-            type: Object,
-            required: true,
-        },
+<script setup>
+const props = defineProps({
+    currentProperty: {
+        type: String,
+        required: true,
     },
-    data() {
-        return {
-            filtersList: this.filterTypes,
-        }
+    property: {
+        type: String,
+        required: true,
     },
-    computed: {
-        sortedFiltersList() {
-            const filtersEntries = Object.entries(this.filtersList)
-
-            // Separating unknown so that it can appear first
-            const unknownFilterEntry = filtersEntries.find(
-                (entry) => entry[1].label === 'Unknown'
-            )
-            const restFiltersEntries = filtersEntries.filter(
-                (entry) => entry[1].label !== 'Unknown'
-            )
-
-            // Sorting rest of filters into alphabetical order
-            restFiltersEntries.sort((a, b) =>
-                a[1].label.localeCompare(b[1].label)
-            )
-
-            // Recombining
-            const sortedFiltersEntries = unknownFilterEntry
-                ? [unknownFilterEntry, ...restFiltersEntries]
-                : restFiltersEntries
-            return Object.fromEntries(sortedFiltersEntries)
-        },
+    propertyLabel: {
+        type: String,
+        required: true,
     },
-    methods: {
-        setFilterInactive: function (filterType) {
-            this.filtersList[filterType].active = false
-        },
-        setFilterActive: function (filterType) {
-            this.filtersList[filterType].active = true
-        },
-        filterEmit: function (filterType) {
-            let isActive = this.filtersList[filterType].active
-
-            if (isActive) {
-                this.setFilterInactive(filterType)
-                this.$emit('filterOff', [this.property, filterType])
-            } else {
-                this.setFilterActive(filterType)
-                this.$emit('filterOn', [this.property, filterType])
-            }
-        },
-        emitSetPropertyToCurrent: function () {
-            this.$emit('setPropertyToCurrent', this.property)
-        },
+    filterTypes: {
+        type: Object,
+        required: true,
     },
+})
+
+const filtersList = ref(props.filterTypes)
+
+const setFilterInactive = (filterType) => {
+    filtersList.value[filterType].active = false
 }
-</script>
 
-<style></style>
+const setFilterActive = (filterType) => {
+    filtersList.value[filterType].active = true
+}
+
+const emit = defineEmits(['filterOff', 'filterOn', 'setPropertyToCurrent'])
+
+const filterEmit = (filterType) => {
+    const isActive = filtersList.value[filterType].active
+
+    if (isActive) {
+        setFilterInactive(filterType)
+        emit('filterOff', [props.property, filterType])
+    } else {
+        setFilterActive(filterType)
+        emit('filterOn', [props.property, filterType])
+    }
+}
+
+const emitSetPropertyToCurrent = () => {
+    emit('setPropertyToCurrent', props.property)
+}
+
+const sortedFiltersList = computed(() => {
+    const filtersEntries = Object.entries(filtersList.value)
+
+    // Separating unknown so that it can appear first
+    const unknownFilterEntry = filtersEntries.find(
+        (entry) => entry[1].label === 'Unknown'
+    )
+    const restFiltersEntries = filtersEntries.filter(
+        (entry) => entry[1].label !== 'Unknown'
+    )
+
+    // Sorting rest of filters into alphabetical order
+    restFiltersEntries.sort((a, b) => a[1].label.localeCompare(b[1].label))
+
+    // Recombining
+    const sortedFiltersEntries = unknownFilterEntry
+        ? [unknownFilterEntry, ...restFiltersEntries]
+        : restFiltersEntries
+    return Object.fromEntries(sortedFiltersEntries)
+})
+</script>
