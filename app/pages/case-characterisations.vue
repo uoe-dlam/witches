c13519cbf7c5cf85c83df1bcb160d80907f8aa2c
Leopold Ramutsamaya <leoramsy@gmail.com>
2026-07-01 13:17:47 +0100

    refactor case characterisations page

--- app/pages/case-characterisations.vue
+++ app/pages/case-characterisations.vue
@@ -2,16 +2,16 @@
     <loading-message v-if="loading" />
     <map-component
         v-else
-        :pageInfo="pageInfo"
-        :originalMarkers="originalMarkers"
-        :filtersGeneralInfo="filtersGeneralInfo"
-        :filterProperties="filterProperties"
-        iconBehaviour="constant"
+        :filter-properties="filterProperties"
+        :filters-general-info="filtersGeneralInfo"
+        :original-markers="originalMarkers"
+        :page-info="pageInfo"
+        icon-behaviour="constant"
     >
     </map-component>
 </template>
 
-<script>
+<script setup>
 import { SPARQLQueryDispatcher } from '~/assets/js/SPARQLQueryDispatcher'
 import APIDataHandler from '~/assets/js/APIDataHandler'
 import json from '../big-query-output.json'
@@ -24,147 +24,139 @@ definePageMeta({
     layout: 'default',
 })
 
-export default {
-    components: { MapComponent, LoadingMessage },
-    data: () => ({
-        pageInfo: {
-            title: 'Primary and Secondary Characterisations',
-            html: '<div>This map shows the geographical residence location for each accused witch in Scotland taken from the Survey of Scottish Witchcraft Database. Out of the <strong>3212</strong> accused witches whose names are known, the residence for <strong>3142</strong> witches has been located. The majority of the residences are accurately located down to the precise settlement, while others range from parish to county depending on the records surviving for each accused witch. There is a total of 821 different locations recorded in the database; all but 25 of these have been identified. The remaining unidentified place-names are currently recorded as \‘County of’\ on the map.</div>',
-            footer: 'witches.is.ed.ac.uk',
-            confirmButtonText: 'Close',
-            type: 'info',
-            showCloseButton: true,
-        },
-        queryOutput: json,
-        sparqlUrl: 'https://query.wikidata.org/sparql',
-        wikiPages: [],
-        loading: true,
-        originalMarkers: [],
-        filtersToFind: [
-            ['primary', 'constant'],
-            ['secondary', 'constant'],
-        ],
-        filtersGeneralInfo: {
-            title: 'Case characterisations',
-            filtersShowing: true,
-        },
-        filterProperties: {
-            primary: {
-                label: 'Primary',
-                description: '',
-                descriptionShowing: false,
-                filters: {},
-                showing: true,
-            },
-            secondary: {
-                label: 'Secondary',
-                description: '',
-                descriptionShowing: false,
-                filters: {},
-                showing: false,
-            },
-        },
-    }),
-    methods: {
-        loadWikiEntries: function () {
-            const sparqlQuery = `SELECT DISTINCT ?item ?LabelEN ?page_title
+const pageInfo = ref({
+    title: 'Primary and Secondary Characterisations',
+    html: '<div>This map shows the geographical residence location for each accused witch in Scotland taken from the Survey of Scottish Witchcraft Database. Out of the <strong>3212</strong> accused witches whose names are known, the residence for <strong>3142</strong> witches has been located. The majority of the residences are accurately located down to the precise settlement, while others range from parish to county depending on the records surviving for each accused witch. There is a total of 821 different locations recorded in the database; all but 25 of these have been identified. The remaining unidentified place-names are currently recorded as \‘County of’\ on the map.</div>',
+    footer: 'witches.is.ed.ac.uk',
+    confirmButtonText: 'Close',
+    type: 'info',
+    showCloseButton: true,
+})
+const queryOutput = ref(json)
+const sparqlUrl = ref('https://query.wikidata.org/sparql')
+const wikiPages = ref([])
+const loading = ref(true)
+const originalMarkers = ref([])
+const filtersToFind = ref([
+    ['primary', 'constant'],
+    ['secondary', 'constant'],
+])
+const filtersGeneralInfo = ref({
+    title: 'Case characterisations',
+    filtersShowing: true,
+})
+const filterProperties = ref({
+    primary: {
+        label: 'Primary',
+        description: '',
+        descriptionShowing: false,
+        filters: {},
+        showing: true,
+    },
+    secondary: {
+        label: 'Secondary',
+        description: '',
+        descriptionShowing: false,
+        filters: {},
+        showing: false,
+    },
+})
+
+const loadWikiEntries = () => {
+    const sparqlQuery = `SELECT DISTINCT ?item ?LabelEN ?page_title
             WHERE {
               ?item wdt:P4478 ?witch .
               ?article schema:about ?item ; schema:isPartOf <https://en.wikipedia.org/> ;  schema:name ?page_title .
               ?item rdfs:label ?LabelEN filter (lang(?LabelEN) = "en") .
             }`
 
-            const queryDispatcher = new SPARQLQueryDispatcher(this.sparqlUrl)
-            queryDispatcher.query(sparqlQuery).then((result) => {
-                for (let i = 0; i < result.results.bindings.length; i++) {
-                    let item = result.results.bindings[i]
+    const queryDispatcher = new SPARQLQueryDispatcher(sparqlUrl.value)
 
-                    let wikiPage = {
-                        id: item.item.value,
-                        pageTitle: item.page_title.value,
-                    }
+    queryDispatcher.query(sparqlQuery).then((result) => {
+        for (let i = 0; i < result.results.bindings.length; i++) {
+            const item = result.results.bindings[i]
 
-                    this.wikiPages.push(wikiPage)
-                }
-            })
-        },
-
-        // Local storage functions:
-        hasLocalStorageExpired: function () {
-            let hours = 24 // Reset when storage is more than 24hours
-            let now = new Date().getTime()
-            let setupTime = localStorage.getItem('setupTime')
-
-            return (
-                setupTime === null || now - setupTime > hours * 60 * 60 * 1000
-            )
-        },
-        loadDataFromLocalStorage: function () {
-            this.originalMarkers = JSON.parse(
-                localStorage.getItem('residenceMarkers')
-            )
-            let allFilters = JSON.parse(localStorage.getItem('allFilters'))
-            this.filterProperties.primary.filters = allFilters.primary
-            this.filterProperties.secondary.filters = allFilters.secondary
-            this.setMarkersIcons()
-            console.log(this.filterProperties)
-        },
-        setMarkersIcons: function () {
-            for (let i = 0; i < this.originalMarkers.length; i++) {
-                let marker = this.originalMarkers[i]
-                marker.markerIcon = '/images/witch-single-orange.png'
-            }
-        },
-        loadData: async function () {
-            this.loadWikiEntries()
-            let icon = '/images/witch-single-orange.png'
-
-            try {
-                let response = await myFetch('/main.php?type=primarysecondary')
-                this.queryOutput = response
-            } catch (e) {
-                Swal.fire({
-                    title: 'Server Error',
-                    html: '<div>We are unable to connect to the server to pull in map info. Please refresh the page and try again. If this error persists, please contact <a href="mailto:ltw-apps-dev.ed.ac.uk">ltw-apps-dev.ed.ac.uk</a></div>',
-                    footer: 'witches.is.ed.ac.uk',
-                    confirmButtonText: 'Close',
-                    type: 'error',
-                    showCloseButton: true,
-                })
-
-                return
+            const wikiPage = {
+                id: item.item.value,
+                pageTitle: item.page_title.value,
             }
 
-            let getData = new APIDataHandler(
-                this.queryOutput,
-                this.wikiPages,
-                null,
-                icon
-            )
-            let filtersFound = null
-
-            ;[this.originalMarkers, filtersFound] = getData.loadAccussed(
-                'residence',
-                this.filtersToFind
-            )
-
-            this.filterProperties.primary.filters = filtersFound.primary
-            this.filterProperties.secondary.filters = filtersFound.secondary
-            this.setMarkersIcons()
-            this.loading = false
-        },
-    },
-
-    mounted: function () {
-        // Load descriptions from the JSON file
-        Object.keys(filterDescriptions).forEach((key) => {
-            if (this.filterProperties[key]) {
-                this.filterProperties[key].description = filterDescriptions[key]
-            }
-        })
-        this.loadData()
-    },
+            wikiPages.value.push(wikiPage)
+        }
+    })
 }
-</script>
 
-<style></style>
+const loadDataFromLocalStorage = () => {
+    originalMarkers.value = JSON.parse(localStorage.getItem('residenceMarkers'))
+    const allFilters = JSON.parse(localStorage.getItem('allFilters'))
+    filterProperties.value.primary.filters = allFilters.primary
+    filterProperties.value.secondary.filters = allFilters.secondary
+
+    setMarkersIcons()
+}
+
+const setMarkersIcons = () => {
+    for (let i = 0; i < originalMarkers.value.length; i++) {
+        const marker = originalMarkers.value[i]
+        marker.markerIcon = '/images/witch-single-orange.png'
+    }
+}
+
+const loadData = async () => {
+    loadWikiEntries()
+    const icon = '/images/witch-single-orange.png'
+
+    try {
+        queryOutput.value = await myFetch('/main.php?type=primarysecondary')
+    } catch {
+        Swal.fire({
+            title: 'Server Error',
+            html: '<div>We are unable to connect to the server to pull in map info. Please refresh the page and try again. If this error persists, please contact <a href="mailto:ltw-apps-dev.ed.ac.uk">ltw-apps-dev.ed.ac.uk</a></div>',
+            footer: 'witches.is.ed.ac.uk',
+            confirmButtonText: 'Close',
+            type: 'error',
+            showCloseButton: true,
+        })
+
+        return
+    }
+
+    const getData = new APIDataHandler(
+        queryOutput.value,
+        wikiPages.value,
+        null,
+        icon
+    )
+    let filtersFound = null
+
+    ;[originalMarkers.value, filtersFound] = getData.loadAccussed(
+        'residence',
+        filtersToFind.value
+    )
+
+    filterProperties.value.primary.filters = filtersFound.primary
+    filterProperties.value.secondary.filters = filtersFound.secondary
+
+    setMarkersIcons()
+    loading.value = false
+}
+
+const hasLocalStorageExpired = () => {
+    const hours = 24 // Reset when storage is more than 24hours
+    const now = new Date().getTime()
+    const setupTime = localStorage.getItem('setupTime')
+
+    return setupTime === null || now - setupTime > hours * 60 * 60 * 1000
+}
+
+onMounted(() => {
+    // Load descriptions from the JSON file
+    Object.keys(filterDescriptions).forEach((key) => {
+        if (filterProperties.value[key]) {
+            filterProperties.value[key].description = filterDescriptions[key]
+        }
+    })
+
+    loadData()
+})
+</script>
