773d2e2b3cc180b20e1b92d485a3e7599e29bd4e
Leopold Ramutsamaya <leoramsy@gmail.com>
2026-07-02 12:00:57 +0100

    update story-isobel-young page

--- app/pages/story-isobel-young.vue
+++ app/pages/story-isobel-young.vue
@@ -1,26 +1,17 @@
 <template>
     <div class="h-full min-h-[500px]">
         <iframe
+            frameborder="0"
+            height="100%"
+            scrolling="no"
             src="https://emcandre.github.io/leaflet-storymaps-with-google-sheets/"
             width="100%"
-            height="100%"
-            frameborder="0"
-            scrolling="no"
         ></iframe>
     </div>
 </template>
 
-<script>
+<script setup>
 definePageMeta({
     layout: 'basic',
 })
-
-export default {
-    data: () => ({}),
-    methods: {},
-    computed: {},
-    mounted: function () {},
-}
 </script>
-
-<style></style>
