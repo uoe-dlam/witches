6bd8ed035874332994eb9d4e429dba62d25a38d6
Leopold Ramutsamaya <leoramsy@gmail.com>
2026-07-02 13:04:22 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,12 +33,8 @@ export default defineNuxtConfig({
         { src: '~/plugins/vue-gtag', mode: 'client' },
         { src: '~/plugins/leaflet', mode: 'client' },
     ],
-    /*
-     ** Nuxt.js modules
-     */
     modules: [
-        //'vue-sweetalert2/nuxt', NOT COMPATATIBLE WITH NUXT 3 YET
-        '@nuxtjs/tailwindcss', // 'nuxt-webfontloader', NOT COMPATATIBLE WITH NUXT 3 YET
+        '@nuxtjs/tailwindcss',
         'nuxt-font-loader',
         '@nuxtjs/leaflet',
         '@nuxt/eslint',
@@ -46,33 +42,21 @@ export default defineNuxtConfig({
     build: {
         transpile: ['@vuepic/vue-datepicker'],
     },
-    purgeCSS: {
-        whitelist: ['lvml'],
-        whitelistPatterns: [/leaflet-.+$/],
-        whitelistPatternsChildren: [/leaflet-.+$/],
+    vite: {
+        optimizeDeps: {
+            include: ['leaflet', 'leaflet.markercluster', 'vue-gtag'],
+        },
     },
     fontLoader: {
         external: [
             {
                 src: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
                 family: 'Roboto',
-                weight: '300 400 500',
             },
             {
                 src: 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
                 family: 'EB Garamond',
-                weight: '400 700',
             },
         ],
-    },
-
-    /*
-     ** Build configuration
-     */
-    build: {
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {},
     },
 })
