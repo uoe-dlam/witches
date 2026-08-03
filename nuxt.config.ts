b4daafc6949b2b01b96d9a291fa91f7fdbeeca1a
dhyeyym <s2774036@ed.ac.uk>
2026-08-03 10:19:44 +0100

    updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,21 @@ export default defineNuxtConfig({
                 'leaflet',
                 'leaflet.markercluster',
                 'sweetalert2',
-                'vue-gtag',],
+                'vue-gtag',
+            ],
+        },
+        build: {
+            rollupOptions: {
+                onwarn(warning, defaultHandler) {
+                    if (
+                        typeof warning.message === 'string' &&
+                        warning.message.includes('check-if-page-unused')
+                    ) {
+                        return
+                    }
+                    defaultHandler(warning)
+                },
+            },
         },
     },
     fontLoader: {
